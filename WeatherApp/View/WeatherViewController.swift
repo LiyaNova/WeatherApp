1a807acfeeb52e3bfeb0a608e1ce38ed848e8472
Yuliya Filimonova <yuliya_nova@icloud.com>
2024-09-05 15:03:22 -0500

    built ui for weather screen

--- WeatherApp/View/WeatherViewController.swift
+++ WeatherApp/View/WeatherViewController.swift
@@ -10,7 +10,54 @@ import UIKit
 
 class WeatherViewController: UIViewController {
     
-    let viewModel: AppViewModelProtocol
+    private let searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.backgroundColor = .white
+        
+        return searchTextField
+    }()
+    
+    private let locationBtn: UIButton = {
+        let locationBtn = UIButton()
+        locationBtn.translatesAutoresizingMaskIntoConstraints = false
+        locationBtn.setImage(UIImage(systemName:"location.circle.fill"), for: .normal)
+        
+        return locationBtn
+    }()
+    
+    private let tempLbl: UILabel = {
+        let tempLbl = UILabel()
+        tempLbl.font = UIFont.systemFont(ofSize: 45, weight: .bold)
+        tempLbl.textAlignment = .right
+        
+        return tempLbl
+    }()
+    
+    private let tempImg: UIImageView = {
+        let tempImg = UIImageView()
+        tempImg.contentMode = .scaleAspectFill
+        
+        return tempImg
+    }()
+    
+    private let searchStack: UIStackView = {
+        let searchStack = UIStackView()
+        searchStack.translatesAutoresizingMaskIntoConstraints = false
+        searchStack.axis = .horizontal
+        
+        return searchStack
+    }()
+    
+    private let tempStack: UIStackView = {
+        let tempStack = UIStackView()
+        tempStack.translatesAutoresizingMaskIntoConstraints = false
+        tempStack.axis = .horizontal
+        
+        return tempStack
+    }()
+    
+    private let viewModel: AppViewModelProtocol
     
     init(viewModel: AppViewModelProtocol) {
         self.viewModel = viewModel
@@ -25,13 +72,35 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUI()
-        viewModel.getUserLocation()
+        setupUI()
+        setupConstraints()
         
+        viewModel.getUserLocation()
     }
     
-    private func setUI() {
-        view.backgroundColor = .cyan
+    private func setupUI() {
+        view.backgroundColor = .lightGray
+    
+        searchStack.addArrangedSubview(locationBtn)
+        searchStack.addArrangedSubview(searchTextField)
+        tempStack.addArrangedSubview(tempLbl)
+        tempStack.addArrangedSubview(tempImg)
+        view.addSubview(searchStack)
+        view.addSubview(tempStack)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            locationBtn.widthAnchor.constraint(equalToConstant: 50),
+            searchTextField.heightAnchor.constraint(equalToConstant: 35),
+            tempImg.widthAnchor.constraint(equalToConstant: 50),
+            searchStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            searchStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            searchStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            tempStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+            tempStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            tempStack.topAnchor.constraint(equalTo: searchStack.bottomAnchor, constant: 20)
+        ])
     }
 }
 
